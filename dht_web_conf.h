18c8b962072adc7c7176231365a43553b68cd38d
pessonnier <loic.pessonnier@gmail.com>
2017-01-12 12:57:02 +0100

    Create dht_web_conf.h

---
+++ dht_web_conf.h
@@ -0,0 +1,4 @@
+#define DHTPIN D3     // pate du vers le data du DHT
+#define DHTTYPE DHT11 // modèle DHT11 ou DHT22
+#define ADR_I2C_SSD1306 0x3C // adresse I2C de l'écran OLED
+#define position "Chambre" // titre de la page web
